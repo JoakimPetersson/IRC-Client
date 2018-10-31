efa4163cfb2712882ef801e3beef3693da9ed727
eriks <eriks@DESKTOP-F4BE4EE>
2018-10-31 11:14:03 +0100

    Added relative path to FXML-file

--- IRC-Client/src/gui/Main.java
+++ IRC-Client/src/gui/Main.java
@@ -1,8 +1,7 @@
 package gui;
 
+import java.io.File;
 import java.net.URL;
-
-import javax.print.attribute.standard.PrinterMoreInfoManufacturer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,15 +18,17 @@ import javafx.stage.Stage;
 	@Override
 	public void start (Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(new URL("file://F:/Programering/Java/IRCclient/IRC-Client/IRC-Client/src/gui/MainWindow.fxml"));
+		String path = new File("").getAbsolutePath();
+		String fullPath = "file:" + File.separator + File.separator + File.separator + path + 
+				File.separator+ "src" + File.separator + "gui" + File.separator + "MainWindow.fxml";
+		System.out.println(fullPath);
+		loader.setLocation(new URL(fullPath));
 		VBox vbox = loader.<VBox>load();
 		
 		Scene scene = new Scene(vbox);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-	}
-	
-	
+	}	
 
 }
