5838dbc5331f44fc1615b06be811ca2f9983e7be
Joakim Petersson <petersson.joakim@gmail.com>
2018-10-31 11:15:08 +0100

    Added functions to connectionhandler

--- IRC-Client/src/network/ConnectionHandler.java
+++ IRC-Client/src/network/ConnectionHandler.java
@@ -1,7 +1,38 @@
 package network;
 
-public class ConnectionHandler {
-	void sendMessage() {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public abstract class ConnectionHandler implements Runnable {
+	private Socket server;
+	private OutputStream out;
+	private ServerInfo info;
+	
+	ConnectionHandler(String serverName, int port){
+		try {
+			server = new Socket(serverName, port);
+			out = server.getOutputStream();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	void sendMessage(String message) {
+		byte[] bytes = (message + "\r\n").getBytes();
+		
+		try {
+			out.write(bytes);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
