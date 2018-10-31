28e1670ea797a7adcf440b18832611d567d00f9a
eriks <eriks@DESKTOP-F4BE4EE>
2018-10-31 10:16:59 +0100

    First window added

--- IRC-Client/src/gui/Main.java
+++ IRC-Client/src/gui/Main.java
@@ -1,5 +1,19 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start (Stage primaryStage) throws Exception {
+		primaryStage.setTitle("This is a title");
+		
+		primaryStage.show();
+	}
+	
+	public static void main (String[] args) {
+		Application.launch(args);
+	}
 
 }
