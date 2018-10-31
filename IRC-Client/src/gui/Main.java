6bd45bd86e681f2175ac135bc01f4585b7d34922
eriks <eriks@DESKTOP-F4BE4EE>
2018-10-31 10:29:56 +0100

    Added FXML

--- IRC-Client/src/gui/Main.java
+++ IRC-Client/src/gui/Main.java
@@ -1,15 +1,19 @@
 package gui;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	@Override
 	public void start (Stage primaryStage) throws Exception {
-		primaryStage.setTitle("This is a title");
-		
-		primaryStage.show();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(new URL("file://F:/Programering/Java/IRCclient/IRC-Client/IRC-Client/src/gui/MainWindow.fxml"));
+		VBox vbox = loader.<VBox>load();
 	}
 	
 	public static void main (String[] args) {
