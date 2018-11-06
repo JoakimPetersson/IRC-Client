73640fbe653f26ffc65499055f5e09669acc4c1d
eriks <eriks@DESKTOP-F4BE4EE>
2018-11-06 10:50:13 +0100

    Made changes to the mainwindow

--- IRC-Client/src/gui/Helper.java
+++ IRC-Client/src/gui/Helper.java
@@ -1,9 +1,18 @@
 package gui;
 
+import javafx.scene.control.TreeItem;
+
 class Helper {
 	
 	public static boolean isEmptyOrNull(final String s) {
 		return s == null || s.trim().isEmpty();		
+	}
+	
+	//Generates a new branch for the server-treeview
+	public static TreeItem<String> makeBranch(String title, TreeItem<String> parent){
+			TreeItem<String> item = new TreeItem<String>(title);
+			parent.getChildren().add(item);
+			return item;
 	}
 
 }
